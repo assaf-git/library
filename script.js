cb944d0a9443835f0663f7ce5a7b6bdeae4d7c9e
Assaf <assafyechiel@gmail.com>
2024-04-19 18:08:13 +0300

    Create addBookToLibrary function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, numberOfPages, read) {
     this.title = title;
     this.author = author;
@@ -7,11 +9,28 @@ function Book(title, author, numberOfPages, read) {
         return title + ', ' + author + ', ' + numberOfPages + ', ' + read;
     }    
 }
-let answer = prompt("Have you read this book?", "Yes");
-if (answer == "Yes") {
-    read = 'Read';
-} else {
-    read = 'Not yet read';
+
+function addBookToLibrary() {
+    let title = prompt("Title: ");
+    let author = prompt("Author: ");
+    let numberOfPages = prompt("Number of pages: ");
+    let read = prompt("Have you read this book? ", "Yes");
+    const newBook = new Book(title, author, numberOfPages, read);
+    console.log(newBook.info());
+    myLibrary.push(newBook);
 }
-const book1 = new Book('The Hobbit', 'JRR Tolkien', '295 pages', read);
-console.log(book1.info());
+
+addBookToLibrary();
+
+console.log(myLibrary);
+
+
+// let answer = prompt("Have you read this book?", "Yes");
+// if (answer == "Yes") {
+//     read = 'Read';
+// } else {
+//     read = 'Not yet read';
+// }
+
+// const book1 = new Book('The Hobbit', 'JRR Tolkien', '295 pages', read);
+// console.log(book1.info());
