3c2a45a75026400ced447bad943bf0c289c8cb32
Assaf <assafyechiel@gmail.com>
2024-04-20 14:09:37 +0300

    Add conditional to addBookToLibrary()

--- script.js
+++ script.js
@@ -30,12 +30,18 @@ function addBookToLibrary() {
     let title = prompt("Title: ");
     let author = prompt("Author: ");
     let numberOfPages = prompt("Number of pages: ");
-    let read = prompt("Have you read this book? ", "Yes");
+    let answer = prompt("Have you read this book?", "Yes");
+    if (answer == "Yes") {
+        read = 'Read';
+    } else {
+        read = 'Not yet read';
+    }
     const newBook = new Book(title, author, numberOfPages, read);
     console.log(newBook.info());
     myLibrary.push(newBook);
 }
 addBookToLibrary();
+
 
 function displayBooks() {
     for (i = 0; i < myLibrary.length; i++) {
@@ -44,23 +50,3 @@ function displayBooks() {
     }
 }
 displayBooks();
-
-console.log(myLibrary);
-
-myLibrary.forEach(function(book){
-    console.log(book.info());
-
-    // let i = 1;
-    // card[i].textContent = "Book: " + book.info();
-    // do {
-
-    // }
-});
-
-
-// let answer = prompt("Have you read this book?", "Yes");
-// if (answer == "Yes") {
-//     read = 'Read';
-// } else {
-//     read = 'Not yet read';
-// }
