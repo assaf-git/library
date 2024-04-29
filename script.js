1728dbe6e5b43a6651c9341aa73c59c5067dd513
Assaf <assafyechiel@gmail.com>
2024-04-29 20:24:19 +0300

    Create makeCard()

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const myLibrary = [];
+const myCards = [];
 
 
-const nodeList = document.querySelectorAll(".card");
-const myCards = Array.from(nodeList);
+// const nodeList = document.querySelectorAll(".card");
+// const myCards = Array.from(nodeList);
 
-// const cardContainer = document.querySelector(".card-container")
+const cardContainer = document.querySelector(".card-container")
 const addBook = document.querySelector(".btn-add-book");
 const formDialog = document.querySelector("#form-dialog");
 const formContainer = document.querySelector(".form-container");
@@ -26,16 +27,17 @@ submitBtn.addEventListener('click', (event) => {
     formDialog.close();
 });
 
-const book1 = new Book("Zero To One", "Peter Thiel", "295", "Read");
-myLibrary.push(book1);
-const book2 = new Book("Never Split The Difference", "Chris Voss", "300", "Read");
-myLibrary.push(book2);
-const book3 = new Book("This Is Marketing", "Seth Godin", "350", "Not yet read");
-myLibrary.push(book3);
-const book4 = new Book("Can't Hurt Me", "David Goggins", "305", "Read");
-myLibrary.push(book4);
+// const book1 = new Book("Zero To One", "Peter Thiel", "295", "Read");
+// myLibrary.push(book1);
+// const book2 = new Book("Never Split The Difference", "Chris Voss", "300", "Read");
+// myLibrary.push(book2);
+// const book3 = new Book("This Is Marketing", "Seth Godin", "350", "Not yet read");
+// myLibrary.push(book3);
+// const book4 = new Book("Can't Hurt Me", "David Goggins", "305", "Read");
+// myLibrary.push(book4);
 
 
+// new book object constructor
 function Book(title, author, numberOfPages, read) {
     this.title = title;
     this.author = author;
@@ -46,7 +48,9 @@ function Book(title, author, numberOfPages, read) {
     }    
 }
 
-
+// assigns values from form to respective variables
+// creates a new book object from those values
+// pushes that new book object into library array
 function addBookToLibrary() {
     let title = formTitle.value;
     let author = formAuthor.value;
@@ -57,17 +61,25 @@ function addBookToLibrary() {
     } else {
         read = 'Not yet read';
     }
-    const newBook = new Book(title, author, numberOfPages, read);
+    newBook = new Book(title, author, numberOfPages, read);
     console.log(newBook.info());
     myLibrary.push(newBook);
-    displayBooks();
+    makeCard();
 }
 
-
+// might not be relevant??
 function displayBooks() {
-    for (i = 0; i < myLibrary.length; i++) {
+    for (i = 0; i < myLibrary.length + 1; i++) {
         console.log(myLibrary[i].info());
-        myCards[i].textContent = "Book: " + myLibrary[i].info();
+        newCard.textContent = "Book: " + myLibrary[i].info();
     }
 }
-displayBooks();
+
+// makes new card and displays book
+function makeCard() {
+    newCard = document.createElement("div");
+    cardContainer.appendChild(newCard).className = "card";
+    for (let book of myLibrary) {
+        newCard.textContent = "Book: " + book.info();
+    }
+}
